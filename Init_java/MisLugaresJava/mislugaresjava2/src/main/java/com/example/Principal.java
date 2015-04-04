3c03f813f19515867db360258e3f81c0b2c30acb
jafamo <jafamo@gmail.com>
2015-04-04 14:32:33 +0100

    Add Mis Lugares class with methods

---
+++ Init_java/MisLugaresJava/mislugaresjava2/src/main/java/com/example/Principal.java
@@ -0,0 +1,16 @@
+package com.example;
+
+/**
+ * Created by jafamo on 04/04/15.
+ */
+public class Principal {
+
+    public static void main(String[] main) {
+        Lugar lugar = new Lugar("Escuela Politécnica Superior de Gandía",
+                "C/ Paranimf, 1 46730 Gandia (SPAIN)", -0.166093, 38.995656,
+                962849300, "http://www.epsg.upv.es",
+                "Uno de los mejores lugares para formarse.", 3);
+        System.out.println("Lugar " + lugar.toString());
+    }
+
+}

99576ca9223d56dfe427faf2efa72be21dc13d03
jafamo <jafamo@gmail.com>
2015-04-29 12:54:10 +0100

    Create "Mis Lugares" with main menu

--- MisLugares/app/src/main/java/android/proyectos/jafamo/mislugares/activity_main.java
+++ MisLugares/app/src/main/java/android/proyectos/jafamo/mislugares/activity_main.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class EdicionLugarActivity extends ActionBarActivity {
+public class activity_main extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

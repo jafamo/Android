79296f2898a8783c9668b7a153bf21d791f9ef17
jafamo <jafamo@gmail.com>
2015-05-04 09:29:23 +0100

    create new String.xml with language english for app (menu init)

--- MisLugares/app/src/main/java/android/proyectos/jafamo/mislugares/activity_main.java
+++ MisLugares/app/src/main/java/android/proyectos/jafamo/mislugares/activity_main.java
@@ -11,7 +11,7 @@ public class activity_main extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.edicion_lugar);
+        setContentView(R.layout.activity_main);
     }
 
 
